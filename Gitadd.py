1c6db418dcd4a9a3bd288405a1e67952946f79d2
Ishita Gupta <guptaishita9999@gmail.com>
2020-08-10 20:42:00 +0530

    0.0093700565616968

--- Gitadd.py
+++ Gitadd.py
@@ -14,5 +14,5 @@ os.system("touch /root/Desktop/workspace/GithubPush/a")
 os.system("git --git-dir=/root/Desktop/workspace/GithubPush/.git --work-tree=/root/Desktop/workspace/GithubPush/ add .")
 
 os.system("git --git-dir=/root/Desktop/workspace/GithubPush/.git --work-tree=/root/Desktop/workspace/GithubPush/ commit -m {}".format(n4)) 
-os.system("git --git-dir=/root/Desktop/workspace/GithubPush/.git --work-tree=/root/Desktop/workspace/GithubPush/ push")
+os.system("git --git-dir=/root/Desktop/workspace/GithubPush/.git --work-tree=/root/Desktop/workspace/GithubPush/ push git HEAD:master")
 
